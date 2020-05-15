1481c5fef3340fa25ffab30dd9c53d299d77c3af
karenw1004 <karenw0103@gmail.com>
2020-05-15 10:23:21 -0500

    Delete all template

--- HM/blueprints/auth.py
+++ HM/blueprints/auth.py
@@ -64,34 +64,3 @@ def logout():
     session.clear()
     flash('You have logged out ','info')
     return redirect(url_for('auth.login'))
-
-
-
-@auth.route('/dashboard', methods=['GET','POST'])
-def dashboard():
-    if (is_logged_in()):
-        count_dict = database().number_dict()
-        return render_template('dashboard.html', count_dict=count_dict)
-    else:
-        flash('You are not logged in ','warning')
-        return redirect("/login")
-
-
-@auth.route('/patient')
-def patient():
-    if (is_logged_in()):
-        data = db.get_all_patient_info_list()
-        return render_template('patient.html', data=data)
-    else:
-        flash('You are not logged in ','warning')
-        return redirect("/login")
-
-@auth.route('/treatment')
-def treatment():
-    if (is_logged_in()):
-        data = db.get_all_treatment_list()
-        icon_list = ['viruses','radiation-alt','disease','crutch','dna','capsules','dumbbell','flask','assistive-listening-systems', 'brain']
-        return render_template('treatment.html', data=data, list= icon_list)
-    else:
-        flash('You are not logged in ','warning')
-        return redirect("/login")
